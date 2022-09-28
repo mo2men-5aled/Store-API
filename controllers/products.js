654ca6de73a6cde9e159e5cfd617f9f0fd38f6ac
mo2men-5aled <momenk208@gmail.com>
2022-09-28 19:04:33 +0200

    sorting

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
 
-  const {featured,company,name} = req.query
+  const {featured,company,name,sort} = req.query
   const queryObject ={}
   if(featured){
     queryObject.featured = featured
@@ -20,7 +20,17 @@ const getAllProducts = async (req, res) => {
   if(name){
     queryObject.name = { $regex: name, $options: "i"}
   }
-  const products = await Product.find(queryObject)
+
+  let result = Product.find(queryObject)
+  if(sort){
+    const sortList = sort.split(",").join(" ")
+    result = result.sort(sortList)
+  }
+  else{
+    result = result.sort("")
+  }
+
+  const products = await result
   if(products.length<1){
     res.status(404).json({msg:"No Results Found"})
   }
